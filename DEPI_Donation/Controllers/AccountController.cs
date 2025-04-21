1e0b2376266547f88cba0ddc35e894f276856751
Fareselawady <121154283+Fareselawady@users.noreply.github.com>
2025-04-21 05:29:01 +0200

    failed making login if can an admin or donor

---
+++ DEPI_Donation/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using DEPI_Donation.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace DEPI_Donation.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbcontext db;
+
+        public AccountController(AppDbcontext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+ 
+    }
+}
